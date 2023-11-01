550d38931f8c98968a1df2d92813d0c998fb6da7
Iarley André <128102571+IarleyAndre@users.noreply.github.com>
2023-10-31 21:16:50 -0300

    Add files via upload

---
+++ adicionar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center> 
+        <form id = "form-add" action="#" method="POST">
+          <br> nome :<input type="text" name = "nome" > <br>
+            <form id = "form-add" action="#" method="POST">
+
+          <br> CPF :<input type="text" name = "cpf" > <br>  
+            
+
+          <br> Telefone :<input type="text" name = "telefone" > <br>
+            
+
+          <br> login :<input type="text" name = "login" > <br>
+          
+
+          <br> Senha :<input type="text" name = "senha" > <br>
+
+            <input type="submit" name = "cadastrar" value = "cadastrar" >
+    </center>
+</body>
+</html>
---
+++ principal.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <h1> olá, administrador</h1>
+        <a href="adicionar.php">
+            adicionar usuario</a><br>
+    </center>
+</body>
+</html>
